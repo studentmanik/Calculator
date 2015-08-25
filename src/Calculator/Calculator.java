ec7addcc3ae192d71e3c594febedecd5562dace2
studentmanik <studentmanik@gmail.com>
2015-08-26 02:53:28 +0600

    Calculator assignment done

---
+++ src/Calculator/Calculator.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Calculator;
+
+/**
+ *
+ * @author Md Mamin
+ */
+public class Calculator {
+
+    public double getResult(String f, String l, String s) {
+        double fn=0;
+        double ln =0;
+        if (f != "" ) {
+             fn = Double.parseDouble(f);
+           
+        }if (l != "") {
+              ln = Double.parseDouble(l);
+        }
+
+        double result = 0;
+        switch (s) {
+
+            case "+":
+                result = fn + ln;
+                break;
+            case "-":
+                result = fn - ln;
+                break;
+            case "/":
+                result = fn / ln;
+                break;
+            case "*":
+                result = fn * ln;
+                break;
+        }
+        return result;
+    }
+}
---
+++ src/Calculator/Data.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Calculator;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Data {
+    private int fNumber;
+    private int lNumber;
+
+    public int getfNumber() {
+        return fNumber;
+    }
+
+    public void setfNumber(int fNumber) {
+        this.fNumber = fNumber;
+    }
+
+    public int getlNumber() {
+        return lNumber;
+    }
+
+    public void setlNumber(int lNumber) {
+        this.lNumber = lNumber;
+    }
+    
+    
+}
